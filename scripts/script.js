f0bb95b6762f1704d0e28d8844384d8ab4aca969
AhmedFathy2001 <56750728+AhmedFathy2001@users.noreply.github.com>
2021-12-25 05:09:33 +0200

    added validation on the textarea

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,18 @@ window.addEventListener("load", () => {
 
     const extract = document.getElementById("extract");
     const jsonExtract = document.getElementById("jsonExtract");
-    const symptomsTable = document.getElementById('symptomsTable')
+    const symptomsTable = document.getElementById("symptomsTable")
+    const symptomsTextBox = document.getElementById("symptomsTextBox");
 
     extract.addEventListener("click", () => {
-        jsonExtract.classList.replace("d-none", "d-inlineblock");
-        symptomsTable.classList.replace("d-none", "d-block");
+        if (!symptomsTextBox.value) {
+            symptomsTextBox.classList.add("is-invalid");
+        } else {
+            jsonExtract.classList.replace("d-none", "d-inlineblock");
+            symptomsTable.classList.replace("d-none", "d-block");
+            symptomsTextBox.classList.remove("is-invalid");
+        }
+
     });
+
 });
